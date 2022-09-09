1d10663a66ad3bf6fe5a39b67c62bc643c185a61
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-09-09 19:55:06 +0530

    Added flyway migrations

---
+++ bookmarker-api/src/main/resources/db/migration/postgresql/V2__insert_sample_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO bookmarks (title, url, created_at)
+VALUES ('GitHub', 'https://github.com/', CURRENT_TIMESTAMP),
+       ('JetBrains Academy', 'https://hyperskill.org/study-plan', CURRENT_TIMESTAMP),
+       ('freeCodeCamp', 'https://www.freecodecamp.org/learn', CURRENT_TIMESTAMP),
+       ('Spring Initializr', 'https://start.spring.io/', CURRENT_TIMESTAMP),
+       ('Thymeleaf', 'https://www.thymeleaf.org/', CURRENT_TIMESTAMP),
+       ('Docker Hub', 'https://hub.docker.com/', CURRENT_TIMESTAMP),
+       ('Coursera', 'https://www.coursera.org/', CURRENT_TIMESTAMP),
+       ('MIT OpenCourseWare', 'https://ocw.mit.edu/', CURRENT_TIMESTAMP),
+       ('LeetCode', 'https://leetcode.com/', CURRENT_TIMESTAMP),
+       ('Medium', 'https://medium.com/', CURRENT_TIMESTAMP),
+       ('MDN Web Docs', 'https://developer.mozilla.org/en-US/', CURRENT_TIMESTAMP),
+       ('Git Book', 'https://git-scm.com/book/en/v2', CURRENT_TIMESTAMP),
+       ('Graph Editor', 'https://csacademy.com/app/graph_editor/', CURRENT_TIMESTAMP),
+       ('Java Visualizer', 'https://cscircles.cemc.uwaterloo.ca/java_visualize/', CURRENT_TIMESTAMP),
+       ('regex101', 'https://regex101.com/', CURRENT_TIMESTAMP)
+;
